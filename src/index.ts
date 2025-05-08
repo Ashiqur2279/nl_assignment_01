670202913dd3f91c2d8afcc9f204a5d3c6406b5c
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-05-08 14:09:00 +0600

    initial commit and task 1 and 2 completed

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLocaleLowerCase();
+  } else return input.toUpperCase();
+}
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
