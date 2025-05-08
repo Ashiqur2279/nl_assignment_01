db596007d6c6d2a57248f8e396e9b70f569af062
Ashiqur Rahman <ashiqurrahman2279@gmail.com>
2025-05-08 14:25:26 +0600

    task 03 has been completed

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ function filterByRating(
 ): { title: string; rating: number }[] {
   return items.filter((item) => item.rating >= 4);
 }
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.reduce((acc, cur) => acc.concat(cur), []);
+}
